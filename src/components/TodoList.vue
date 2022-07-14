d8901bdebc91e97e766d780f7867b1daa64dd313
paul <paul.marila91@gmail.com>
2022-07-14 12:36:43 +0300

    cc

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const todos = await response.json();
+    
+</script>
+
+<template>
+<ul>
+    <li v-for="todo of todos" v-bind:key="todo.id">{{ todo.title }}</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+</template>
+
+<style scoped>
+
+</style>
